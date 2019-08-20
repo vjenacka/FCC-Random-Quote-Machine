1cebf8f9825f903720d33b8201dc232abef7fd12
vjenacka <edins613@gmail.com>
2019-08-20 17:16:02 +0200

    Updates

---
+++ precache-manifest.076ebb12354cb0f097acbbcc9ae023c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72d9a4a2f3711d38ddb3df99e91c6958",
+    "url": "/FCC-Random-Quote-Machine/index.html"
+  },
+  {
+    "revision": "294ed9f4b223787f4b0f",
+    "url": "/FCC-Random-Quote-Machine/static/css/main.c1c6ec89.chunk.css"
+  },
+  {
+    "revision": "6d431e16c894dbcdc4bf",
+    "url": "/FCC-Random-Quote-Machine/static/js/2.647d150d.chunk.js"
+  },
+  {
+    "revision": "294ed9f4b223787f4b0f",
+    "url": "/FCC-Random-Quote-Machine/static/js/main.eb9135b2.chunk.js"
+  },
+  {
+    "revision": "a92f9ef0511f3798e476",
+    "url": "/FCC-Random-Quote-Machine/static/js/runtime~main.2f40ea4e.js"
+  }
+]);
