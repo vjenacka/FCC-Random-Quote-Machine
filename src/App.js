6b9312d6d5a38c69713d00d1967e258e769306d8
vjenacka <edins613@gmail.com>
2019-08-20 15:59:40 +0200

    finished the project, now to style it a bit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
 import React from "react";
+import QuoteCard from "./components/QuoteCard/QuoteCard";
 
 class App extends React.Component {
+  state = {
+    quotes: [],
+    quote: "",
+    author: "",
+    isFetched: false
+  };
+  componentDidMount() {
+    fetch(
+      "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
+    )
+      .then(resp => {
+        return resp.json();
+      })
+      .then(jsonResp => {
+        const quoteIndex = Math.floor(Math.random() * 102);
+        this.setState({
+          quotes: jsonResp.quotes,
+          quote: jsonResp.quotes[quoteIndex].quote,
+          author: jsonResp.quotes[quoteIndex].author,
+          isFetched: !this.state.isFetched
+        });
+      });
+  }
+  newQuote = () => {
+    const quoteIndex = Math.floor(Math.random() * 102);
+    this.setState({
+      quote: this.state.quotes[quoteIndex].quote,
+      author: this.state.quotes[quoteIndex].author
+    });
+  };
   render() {
     return (
       <div className="App">
-        <h1>Hello</h1>
+        {this.state.isFetched && (
+          <QuoteCard
+            quote={this.state.quote}
+            author={this.state.author}
+            newQuote={this.newQuote}
+          />
+        )}
       </div>
     );
   }
--- src/components/QuoteCard/QuoteCard.js
+++ src/components/QuoteCard/QuoteCard.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
+import "./QuoteCard.css";
 
-const QuoteCard = () => {
-    return (
-        <div>
-            <h1>Hello world</h1>
-        </div>
-    )
-}
+const QuoteCard = ({ quote, author, newQuote }) => {
+  return (
+    <div id="quote-box">
+      <div className="quote">
+        <i className="fa fa-quote-left" />
+        <span id="text">{quote}</span>
+      </div>
+      <span id="author">- {author}</span>
+      <div className="buttons">
+        <a href="twitter.com/intent/tweet" target="_blank" id="tweet-quote">
+          <i className="fab fa-twitter-square" />
+        </a>
+        <span>
+          <i className="fab fa-tumblr-square" />
+        </span>
+        <span id="new-quote" onClick={newQuote}>
+          New quote
+        </span>
+      </div>
+    </div>
+  );
+};
 
-export default QuoteCard
+export default QuoteCard;
